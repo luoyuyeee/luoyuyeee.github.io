69b3c98fc2cc17051fa77ff33448695e42874832
luoyuyeee <2307389421@qq.com>
2025-06-12 21:59:35 +0800

    品优购-JavaScript-轮播图制作
    1.动态生成小点点 2.小点点的排他思想

--- project03/shopping/js/index.js
+++ project03/shopping/js/index.js
@@ -11,5 +11,28 @@ window.addEventListener('load', function() {
     focus.addEventListener('mouseleave', function() {
         arrow_l.style.display = 'none';
         arrow_r.style.display = 'none';
-    })
+    });
+    // 3.动态生成小点点，根据图片个数动态生成小点点
+    var ul = focus.querySelector('ul');
+    var ol = focus.querySelector('.circle');
+    //验证图片个数
+    // console.log(ul.children.length); 
+    for(var i = 0 ; i < ul.children.length ; i++){
+        // 创建一个 li
+        var li = this.document.createElement('li');
+        // 把小li插入到 ol
+        ol.appendChild(li);
+        // 4.小点点的排他思想
+        li.addEventListener('click', function() {
+            // 排他
+            for(var i = 0 ; i < ol.children.length ; i++) {
+                ol.children[i].className = '';
+            }
+            // 利己
+            this.className = 'current';
+        })
+    }
+    // 把ol里面的第一个小点点li设置类名current
+    ol.children[0].className = 'current';
+    
 })
